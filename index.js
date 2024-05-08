076be48ce0c0976504e1ffbb2e8b174984bfe5cb
Nguyen Quan <quannguyen5987@gmail.com>
2024-05-08 11:32:00 +0700

    kdkd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then( window.location.assign("https://www.facebook.com/business"))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
